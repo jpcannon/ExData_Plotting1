614c087275b4c23d4d8d1be3c9ed2f654bb0c915
jcannon61 <jp.cannon@live.com>
2015-09-13 16:57:57 -0500

    Third Plot

---
+++ plot3.R
@@ -0,0 +1,23 @@
+plot3  <-function() {
+  options(warn=-1)
+  suppressMessages(library(chron))
+  suppressMessages(library(lubridate))
+  
+  options(warn=0)
+  
+  dt <- read.csv("household_power_consumption.txt",
+                 header = TRUE, sep = ";",na.strings=c("?"))
+  dt["Date"] <- as.Date(dt$Date,format="%d/%m/%Y")
+  dt <- dt[dt$Date >= "2007-02-01" & dt$Date <= "2007-02-02", ]
+  dt["Time"]<- times(dt$Time)
+
+  plot(wday(dt$Date)+dt$Time,
+       as.numeric(dt$Sub_metering_1),
+       type='l',ylab="Energy sub metering",
+       xlab = "",xaxt = "n")
+  lines(as.numeric(dt$Sub_metering_2),col="red")
+  lines(as.numeric(dt$Sub_metering_3),col="blue")
+  axis(1, at=5:7, labels=c("Thur","Fri","Sat"))
+  dev.copy(png,filename="ExData_Plotting1\\plot3.png");
+  dev.off ();
+}
