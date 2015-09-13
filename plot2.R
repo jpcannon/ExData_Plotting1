b2d50a27448609572ed7c65a7fbf7498a4157f77
jcannon61 <jp.cannon@live.com>
2015-09-13 16:43:42 -0500

    Second Plot

--- plot2.R
+++ plot2.R
@@ -1,16 +1,21 @@
-plot1  <-function() {
+plot2  <-function() {
   options(warn=-1)
   suppressMessages(library(chron))
+  suppressMessages(library(lubridate))
+  
   options(warn=0)
   
-  dt <- read.csv("household_power_consumption.txt",header = TRUE, sep = ";")
+  dt <- read.csv("household_power_consumption.txt",
+                 header = TRUE, sep = ";",na.strings=c("?"))
   dt["Date"] <- as.Date(dt$Date,format="%d/%m/%Y")
   dt <- dt[dt$Date >= "2007-02-01" & dt$Date <= "2007-02-02", ]
   dt["Time"]<- times(dt$Time)
-  hist(as.numeric(dt$Global_active_power)/1000,
-       xlab="Global Active Power (kilowatts)", 
-       main="Global Active Power", col="red",
-       xlim = c(0,6),ylim=c(0,1200))
-  dev.copy(png,filename="plot1.png");
+
+  plot(wday(dt$Date)+dt$Time,
+       as.numeric(dt$Global_active_power),
+       type='l',ylab="Global Active Power(kilowatts)",
+       xlab = "",xaxt = "n")
+  axis(1, at=5:7, labels=c("Thur","Fri","Sat"))
+  dev.copy(png,filename="ExData_Plotting1\\plot2.png");
   dev.off ();
 }
